66b7b8b213a260d69faee3736e521dc7cd7f07c5
Emad Almousa <almousa.emad.92@gmail.com>
2024-06-17 16:17:30 +0200

    searching

--- app/components/Inhalt.tsx
+++ app/components/Inhalt.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import HomeHeader from './HomeHeader'
+import Image from 'next/image'
+import HomeSearch from './HomeSearch'
 
 const Inhalt = () => {
   return (
-    <div>
+    <div className=''>
       <HomeHeader />
-      Inhalt
+      <div className='flex  flex-col items-center mt-24'>
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png"
+          alt="Google Logo"
+          width={300}
+          height={100}
+          priority
+          style={{ width: "auto" }}
+        />
+        <HomeSearch />
+      </div>
     </div>
   )
 }
