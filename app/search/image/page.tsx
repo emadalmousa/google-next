261766f8159ade8717c3e48e9b9d6f222b2d7617
Emad Almousa <almousa.emad.92@gmail.com>
2024-06-19 14:52:45 +0200

    add image

--- app/search/image/page.tsx
+++ app/search/image/page.tsx
@@ -1,8 +1,37 @@
+
+import ImageSearchResults from '@/app/components/ImageSearchResults';
+import Link from 'next/link';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 
-const page = () => {
+interface Props {
+  searchParams: {
+    searchTerm: string;
+  };
+}
+
+const page = async ({ searchParams }: Props) => {
+  const res = await fetch(` https://www.googleapis.com/customsearch/v1?key=${process.env.
+    API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`);
+  if (!res.ok) {
+    throw new Error('Somthing is wrong');
+  }
+  const data = await res.json();
+  const results = data.items;
+  if (!results) {
+    return (
+      <div className='flex flex-col justify-center items-center pt-10'>
+        <h1>No data find</h1>
+        <p className='text-lg'>Try somthing else</p>
+        <Link href='/' className='text-blue-500'>Home</Link>
+      </div>
+    )
+
+  }
   return (
-    <div >page</div>
+    <div >
+      {<ImageSearchResults  results={data}/>}
+    </div>
   )
 }
 
