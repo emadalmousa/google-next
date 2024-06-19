29948c145f0f59e05b4f09d7b19b318bb27833b9
Emad Almousa <almousa.emad.92@gmail.com>
2024-06-19 16:01:30 +0200

    parseInt

--- app/components/PaginationButtons.tsx
+++ app/components/PaginationButtons.tsx
@@ -8,7 +8,7 @@ const PaginationButtons = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const searchTerm = searchParams.get('searchTerm');
-  const startIndex = +searchParams.get('start') || 1;
+  const startIndex = parseInt(searchParams.get('start') || '1', 10);
   return (
     <div className='text-blue-700 flex px-10 pb-4 justify-between
     sm:justify-start sm:space-x-44 sm:px-0'>
